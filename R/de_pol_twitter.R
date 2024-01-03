d3dd8ed6ef61dfe1834e6cf5a040f376023c0d0f
TimBMK <tim.b.m.koenig@web.de>
2024-01-03 18:04:13 +0100

    added party association and tweet URL to the tweet data

--- R/de_pol_twitter.R
+++ R/de_pol_twitter.R
@@ -1,8 +1,10 @@
 #' Sample data of Tokenized Tweets from German Politicians
 #'
-#' Data contains the tokenized tweets of elected German polticians sent between 31st of July and 6th of August 2022. Tokenization and lemmatization via SpaCy's de_core_news_lg model. Lemmas have additionally been set to lower case.
-#' For demonstration purposes in the package, the variable "ministry_name" has been added, with `NA` for all tweets by accounts that do not represent a German ministry.
-#' doc_id describes the tweet ID, author ID the user ID of the authoring account, and created_at the moment of tweeting.
+#' Data contains the tokenized tweets of elected German polticians sent between 31st of July and 6th of August 2022. Tokenization and lemmatization via SpaCy's de_core_news_lg model. 
+#'  Lemmas have additionally been set to lower case. 
+#' `doc_id` describes the tweet ID, `author_id` the user ID of the authoring account, and `created_at` the moment of tweeting. `tweet_url` contains the link to the original tweet. 
+#'  Additionally, the variable `party` has been added, describing the party association of the account in question (`NA` if the account has no party association).
+#' For demonstration purposes in the package, the variable `ministry_name` has been added, with `NA` for all tweets by accounts that do not represent a German ministry.
 #'
 #' @docType data
 #'
