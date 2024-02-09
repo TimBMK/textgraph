1a302738f23db32e7fca606cf81f1fc0a1f3b8e8
TimBMK <tim.b.m.koenig@web.de>
2024-02-09 12:21:20 +0100

    replaced dplyr with data.table syntax for increased efficiency on big data sets

--- R/prepare_document_data.R
+++ R/prepare_document_data.R
@@ -10,10 +10,11 @@
 #' @return prepared document data
 #'
 #' @importFrom dplyr "%>%"
-#' @importFrom dplyr left_join join_by summarise n mutate case_when arrange desc select distinct select_if across
+#' @importFrom dplyr summarise n mutate case_when select distinct select_if across
 #' @importFrom tidytext bind_tf_idf
 #' @importFrom scales rescale
 #' @importFrom tidyselect everything
+#' @importFrom data.table as.data.table setkeyv merge.data.table setorder
 #'
 #' @keywords internal
 
@@ -22,35 +23,55 @@ prepare_document_data <- function(topics,
                                   documents,
                                   document_tokens,
                                   document_ids) {
-  tf_idf <- topics %>%
-    dplyr::left_join(documents,
-                     by = dplyr::join_by(entity == !!as.name(document_tokens)),
-                     relationship = "many-to-many") %>%
-    dplyr::summarise(n = dplyr::n(), # calculate tf_idf
-                     .by = c(entity, !!as.name(document_ids),
-                             topic, page_rank)) %>%
-    tidytext::bind_tf_idf(entity, !!as.name(document_ids), n) %>%
+
+  join_dat <- documents %>% # prepare data for data.table join
+    dplyr::select(!!as.name(document_tokens),
+                  !!as.name(document_ids)) %>%
+    dplyr::rename(entity = !!as.name(document_tokens)) %>%
+    data.table::as.data.table()
+
+  data.table::setkeyv(join_dat, "entity")
+
+  topics <- data.table::as.data.table(topics)
+
+  data.table::setkeyv(topics, "entity")
+
+  tf_idf <- data.table::merge.data.table(topics, join_dat, by = "entity", allow.cartesian = T)
+
+  invisible(gc())
+
+  tf_idf <- tf_idf[ , .N, by = c("entity", document_ids, "topic", "page_rank")] # count duplicated entities
+
+  tf_idf <- tf_idf %>%
+    tidytext::bind_tf_idf(entity, !!as.name(document_ids), N) %>% # calculate tf-idf
     dplyr::mutate(page_rank = dplyr::case_when(is.na(page_rank) ~ 0, # set missing page ranks to 0 (missing if a term has no connection to other terms in page_rank = "cluster")
                                                .default = page_rank)) %>%
     dplyr::mutate(term_relevance = (tf_idf * page_rank) %>% # calculate topic term relevance as normalized (tf_idf * page_rank)
                     scales::rescale(to = c(0, 1)), # rescale
                   .by = topic)
 
+  document_data <- tf_idf[ , .(entities = list(entity),# summarise entities and document relevance
+                               document_relevance = sum(term_relevance)),
+                           by = c("topic", document_ids)]
 
-  document_data <- tf_idf %>%
-    dplyr::summarise(entities = list(entity),
-                     document_relevance = sum(term_relevance), # sum up term relevance per doc
-                     .by = c(topic, !!as.name(document_ids))) %>%
-    dplyr::mutate(document_relevance = scales::rescale(document_relevance, # rescale per topic
-                                                       to = c(0,100)),
-                  .by = topic) %>%
-    dplyr::arrange(topic, dplyr::desc(document_relevance))
+  document_data[ , document_relevance := scales::rescale(document_relevance, # rescale per topic
+                                                         to = c(0,100)),
+                 by = "topic"]
+
+  data.table::setorder(document_data, topic, -document_relevance)
+
+
+  join_dat <- documents %>% # prepare 2nd join
+    dplyr::select(!(!!as.name(document_tokens))) %>%
+    dplyr::distinct(!!as.name(document_ids),
+                    .keep_all = TRUE) %>%
+    data.table::as.data.table()
+
+  data.table::setkeyv(join_dat, document_ids)
+  data.table::setkeyv(document_data, document_ids)
 
   document_data <- document_data %>%
-    dplyr::left_join(documents %>%
-                       dplyr::select(!(!!as.name(document_tokens))) %>%
-                       dplyr::distinct(!!as.name(document_ids),
-                                       .keep_all = TRUE), by = document_ids)
+    data.table::merge.data.table(join_dat, by = document_ids)
 
   missing_data <- document_data %>% # collect missing meta data
     dplyr::select(!c(topic, entities, document_relevance)) %>%
